fc0dfbfe101cf80b4cbba5a7c0e6035e813aa0c6
Feng Yan <2316696377.com>
2021-09-11 20:32:15 +0800

    init

--- tutorial01/tutorial01.c
+++ tutorial01/tutorial01.c
@@ -1,5 +1,6 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* main: generate some simple output */
 
@@ -27,5 +28,27 @@ int main(void) {
 
 	printf("Number of minutes since midnight: %i\n", hour * 60 + minute);
 	printf("Fraction of the hour that has passed: %i\n", minute / 60);
+
+	char letter;
+	letter = 'a' + 1;
+	printf("%c\n", letter);
+	
+	int number;
+	number = 'a';
+	printf("%i\n", number);
+
+	double pi;
+	pi = 3.1415926;
+
+	double y = 1 / 3;
+	printf("%0.3f\n", y);
+
+	const double PI = acos(-1.0);
+	printf("%0.15f\n", PI);
+
+	double loge = log(17.0);
+	double logten = log10(17.0);
+	double sina = sin(1.5);
+
 	return(EXIT_SUCCESS);
 }
